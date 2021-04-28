966706567c2531fd0473c9b63c799b1ed6a95e82
131sassildiko <sass.ildiko@freemail.hu>
2021-04-28 21:45:13 +0200

    correction

--- src/main/java/hu/nive/ujratervezes/zarovizsga/peoplesql/PeopleDao.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/peoplesql/PeopleDao.java
@@ -3,6 +3,7 @@ package hu.nive.ujratervezes.zarovizsga.peoplesql;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -18,15 +19,30 @@ public class PeopleDao {
         try (
                 Connection conn = dataSource.getConnection();
                 PreparedStatement stmt =
-                        conn.prepareStatement("SELECT ip_address FROM people WHERE first_name = ? AND last_name = ?");
-        ) {
+                        conn.prepareStatement("SELECT ip_address FROM people WHERE first_name = ? AND last_name = ?")) {
             stmt.setString(1, firstName);
             stmt.setString(2, lastName);
-            return stmt.toString();
+
+            return findByStatement(stmt);
+
         } catch (SQLException sqle) {
             throw new IllegalArgumentException("Error by insert", sqle);
         }
     }
+
+    public String findByStatement(PreparedStatement stmt) {
+        try (
+                ResultSet rs = stmt.executeQuery();
+        ) {
+            if (rs.next()) {
+                return rs.getString("ip_address");
+            }
+        } catch (SQLException sqle) {
+            throw new IllegalArgumentException("Error by insert", sqle);
+        }
+        throw new IllegalArgumentException("Cannot find");
+    }
+
 }
 
 
